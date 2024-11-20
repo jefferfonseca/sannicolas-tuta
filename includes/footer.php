8c04381153cf3e7ae0534422edf64aad4a30ca02
Ing. Jeffer Fonseca <jefferfonseca@gmail.com>
2024-11-19 19:31:02 -0500

    19-10/24

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 /*  include('includes/db.php');
 $query = "UPDATE Visitas SET Contador = Contador + 1 WHERE Id = '1'";
@@ -21,85 +18,75 @@ echo "<h4 class='center-align'>Número de visitas: 0000" . $row['Contador'] . "<
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <!-- Inicializa los JavaScript -->
 <script>
-   document.addEventListener("DOMContentLoaded", function () {
-       M.AutoInit();
-   });
+    document.addEventListener('DOMContentLoaded', function() {
+        M.AutoInit(); // Inicializa todos los componentes
+    });
 
-   //js fixed header
-   window.onscroll = function () {
-       myFunction();
-   };
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems);
+    });
 
-   var header = document.getElementById("myHeader");
-   var logo = document.getElementById("logo");
-   var sticky = header.offsetTop;
+    //js fixed header
+    window.onscroll = function() {
+        myFunction();
+    };
 
-   function myFunction() {
-       if (window.pageYOffset > sticky) {
-           header.classList.add("sticky");
-           logo.classList.remove("hide");
-       } else {
-           header.classList.remove("sticky");
-           logo.classList.add("hide");
-       }
-   }
+    var header = document.getElementById("myHeader");
+    var logo = document.getElementById("logo");
+    var sticky = header.offsetTop;
 
-   // efecto parallax
-   document.addEventListener("DOMContentLoaded", function () {
-       var elems = document.querySelectorAll(".parallax");
-       var instances = M.Parallax.init(elems);
-   });
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+            logo.classList.remove("hide");
+        } else {
+            header.classList.remove("sticky");
+            logo.classList.add("hide");
+        }
+    }
 
-   // js Slider
-   var slideIndex = 0;
-   showSlides();
+    // efecto parallax
+    document.addEventListener("DOMContentLoaded", function() {
+        var elems = document.querySelectorAll(".parallax");
+        var instances = M.Parallax.init(elems);
+    });
 
-   function showSlides() {
-       var i;
-       var slides = document.getElementsByClassName("mySlides");
-       for (i = 0; i < slides.length; i++) {
-           slides[i].style.display = "none";
-       }
-       slideIndex++;
-       if (slideIndex > slides.length) {
-           slideIndex = 1;
-       }
-       slides[slideIndex - 1].style.display = "block";
-       setTimeout(showSlides, 2000); // Change image every 2 seconds
-   }
+    // js Slider
+    var slideIndex = 0;
+    showSlides();
 
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
 </script>
-
-<footer class="page-footer">
-   <div class="container">
-       <div class="row">
-           <!-- Escudo del colegio -->
-           <div class="col s12 m4 ">
-           </div>
-
-           <!-- Redes Sociales -->
-           <div class="col s12 m4 center">
-               <a href="https://web.facebook.com/ietsannicolas" target="_blank">
-                <img src="./images/redes/fb.png" style="border-radius: 50%; width: 90px; height: 50px;" 
-                alt="@ietsannicolas" /></a>
-           </div>
-           <!-- Contacto -->
-           <div class="col s12 m4">
-               <h4><b>Contáctanos</b></h4>
-               <span>
-                   <h5><b>Celular:</b></h5>
-                   +57 321 222 2082
-                   <br />
-                   <h5><b>Dirección:</b></h5>
-                    Tuta, Boyacá, Colombia
-                   <br />
-                   <br />
-               </span>
-           </div>
-           </div>
-   </div>
-       <div class="footer-copyright valign-wrapper center-align">
-         <h6>© 2023 | <b>Design:</b> <a href="https://ingjefersonfonsecasoto.com.co" target="_blank">&lt;Ing. Jeferson Fonseca Soto&gt;</a></h6>
-       </div>
-   
+<footer class="p-0">
+    <div class="row valign-wrapper m-0">
+        <div class="col s4  center">
+            <h4 class="white-text">Contacto:</h4>
+            <h6><b>Celular:</b> <br>(+57) 321 222 2082</h6>
+            <h6><b>E-mail:</b> <br>tuta_colsannicolas@hotmail.com</h6>
+            <h6><b>Dirección</b> <br>Vda. El Arenal - Tuta</h6>
+        </div>
+        <div class="col s8 p-0 valign-wrapper">
+            <iframe class="p-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.978313165623!2d-73.22735148532549!3d5.716261833606349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6a6e896ba57b19%3A0xa2950f5e2a24f373!2sInstituci%C3%B3n%20Educativa%20San%20Nicolas%2C%20Sede%20Sider%C3%BArgica!5e0!3m2!1ses!2sco!4v1680283187306!5m2!1ses!2sco" style="width: 100%; height: 400px; border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+    <div class="row foot-dev m-0">
+        <div class="col s12">
+            <div class="container center ">
+                <h6>© 2024 | <b>Powered By:</b> <a href="https://ingjefersonfonsecasoto.com.co" target="_blank">&lt; Ing. Jeferson Fonseca Soto &gt;</a></h6>
+            </div>
+        </div>
+    </div>
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -6,129 +6,93 @@
     <meta name="description" content="Sitio Web Oficial de la Institución Educativa Técnica San Nicolás">
     <title>IET San Nicolás - Tuta</title>
     <link rel="icon" href="./images/Escudo.png" type="image/png">
+     <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+ 
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/slider.css">
     <link rel="stylesheet" href="css/style.css">
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Import Google Font-->
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
-    <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
     <header>
-        <!-- <div class="navbar-fixed">  Pega el menu a la parte de arriba -->
         <div class="navbar">
-            <div class="row header">
-                <div class="col s12 m1 l1 center"> <img src="./images\Escudo.png" alt="sanicolas" width=auto height=90px>
+            <div class="row  center valign-wrapper ">
+                <div class="col s12 m4">
+                    <img src="./images/Escudo.png" alt="sanicolas" height="90px">
                 </div>
-                <div class="col s12 m10 l4">
-                    <h5 class="center">Institución Educativa Técnica<br>San Nicolás - Tuta</h5>
-                    <p class="center">"Queremos y podemos"</p>
+                <div class="col s12 m4 center">
+                    <h5 class="center m-0">Institución Educativa Técnica<br>San Nicolás - Tuta</h5>
+                    <p class="center m-0">"Queremos y podemos"</p>
+                </div>
+                <div class="col s12 m4">
+                    <img src="./images/Escudo-tuta.png" alt="tuta" height="90px">
                 </div>
             </div>
-            <nav class=" green darken-3" id="myHeader">
-                <div class="nav-wrapper">
-                    <a class="hide" id="logo"><img style="padding: 10px;" src="./images/Escudo.png" alt="sannicolas" width=auto height=100%></a>
-                    <a href="#" data-target="menu-responsive" class="sidenav-trigger right">
-                        <i class="material-icons">menu</i>
-                    </a>
-                    <!--lista de menus de navegación-->
-                    <ul class="right hide-on-med-and-down">
-                        <li class="<?php if ($menu == "Inicio") {
-                                        echo "active";
-                                    } ?>"><a href="index.php">Inicio</a></li>
-                        <li><a href="#" class="dropdown-trigger" data-target="institucion">Institución
-                                <i class="material-icons right">arrow_drop_down</i>
-                            </a>
-                        </li>
-                        <li class="<?php if ($menu == "TOVARO") {
-                                        echo "active";
-                                    } ?>"><a href="live.php" target="_blank">Radio</a></li>
-                        <li class="<?php if ($menu == "Especialidades") {
-                                        echo "active";
-                                    } ?>"><a href="#" class="dropdown-trigger" data-target="esp">
-                                Especialidades
-                                <i class="material-icons right">arrow_drop_down</i>
-                            </a>
-                        </li>
-                        <li><a href="https://compucol.co/colegios/tutaiesannicolas/" target="_blank">Sistema de Notas</a></li>
-                        <li class="<?php if ($menu == "Contacto") {
-                                        echo "active";
-                                    } ?>"><a href="contacto.php">Contáctenos</a></li>
-                    </ul>
-                </div>
-            </nav>
-            <!--Submenus NO responsives -->
-            <!-- Lista de Institución -->
-            <ul id="institucion" class="dropdown-content white-text" style="background-color: transparent;">
-                <li></li>
-                <li class="li"><a href="nosotros.php">Reseña Histórica</a></li>
-                <li class="li"><a href="simbolos.php">Simbolos Institucionales</a></li>
-                <li class="li"><a href="sedes.php">Sedes</a></li>
-                <li class="li"><a href="docentes.php">Docentes</a></li>
-                <li class="li"><a href="gobierno-escolar.php">Gobierno Escolar</a></li>
-                <li class="li"><a href="proyectos.php">Proyectos</a></li>
-                <li class="li"><a href="#" target="_blank">Psicoorientación</a></li>
-            </ul>
-            <!-- Lista de especialidades ---->
-           <ul id="esp" class="dropdown-content" style="background-color: transparent;">
-              <!--  <li></li>
-               <li class="li"><a href="redes.php" class="white-text">Redes y sistemas</a></li>
-               <li class="li"><a href="salud.php" class="white-text">Salud</a></li>
-               <li class="li"><a href="turismo.php" class="white-text">Turismo</a></li>
-           --> </ul>
-      <!-- Submenus responsives -->
-            <!-- Lista de Institución -->
-            <ul id="institucion-responsive" class="dropdown-content" style="background-color: transparent;">
-                <li></li>
-                <li class="li"><a href="nosotros.php" class="white-text">Reseña Histórica</a></li>
-                <li class="li"><a href="simbolos.php" class="white-text">Simbolos Institucionales</a></li>
-                <li class="li"><a href="sedes.php" class="white-text">Sedes</a></li>
-                <li class="li"><a href="docentes.php" class="white-text">Docentes</a></li>
-                <li class="li"><a href="gobierno-escolar.php" class="white-text">Gobierno Escolar</a></li>
-                <li class="li"><a href="proyectos.php" class="white-text">Proyectos</a></li>
-                <li class="li"><a href="https://tovaro2020.wixsite.com/psicoorientacion" class="white-text" target="_blank">Psicoorientación</a></li>
-            </ul>
-
-            <!-- Lista de especialidades -->
-            <ul id="esp-responsive" class="dropdown-conten" style="background-color: transparent;">
-               <!--  <li></li>
-                <li class="li"><a href="redes.php" class="white-text">Redes y sistemas</a></li>
-                <li class="li"><a href="salud.php" class="white-text">Salud</a></li>
-                <li class="li"><a href="turismo.php" class="white-text">Turismo</a></li>
-            --> </ul>
-
-            <!-- Sidenav -->
-            <ul class="sidenav" id="menu-responsive">
-                <li class="<?php if ($menu == "Inicio") {
-                                echo "active";
-                            } ?>"><a href="index.php">Inicio</a></li>
-                <li class="<?php if ($menu == "Institución") {
-                                echo "active";
-                            } ?>"><a href="#" class="dropdown-trigger" data-target="institucion-responsive">
-                        Institución
-                        <i class="material-icons right">arrow_drop_down</i>
-                    </a>
-                </li>
-                <li class="<?php if ($menu == "TOVARO") {
-                                echo "active";
-                            } ?>"><a href="live.php" target="_blank">Radio</a></li>
-                <li class="<?php if ($menu == "Especialidades") {
-                                echo "active";
-                            } ?>"><a href="#" class="dropdown-trigger" data-target="esp-responsive">
-                        Especialidades
-                        <i class="material-icons right">arrow_drop_down</i>
-                    </a>
-                </li>
-                <li><a href="https://compucol.co/colegios/tutaiesannicolas/" target="_blank">Sistema de Notas</a></li>
-                <li class="<?php if ($menu == "Contacto") {
-                                echo "active";
-                            } ?>"><a href="contacto.php">Contáctenos</a></li>
-            </ul>
+        </div>
+        <!-- Menú de navegación -->
+        <nav id="myHeader" class="nav ">
+            <div class="nav-wrapper">
+                <a href="#" id="logo" class="hide">
+                    <img style="padding: 10px;" src="./images/Escudo.png" alt="sannicolas" height="100%">
+                </a>
+                <a href="#" data-target="menu-responsive" class="sidenav-trigger right">
+                    <i class="material-icons">menu</i>
+                </a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li>
+                        <a href="#" class="dropdown-trigger" data-target="institucion">Institución
+                            <i class="material-icons right">arrow_drop_down</i>
+                        </a>
+                    </li>
+                    <li><a href="live.php" target="_blank">Radio</a></li>
+                    <li>
+                        <a href="#" class="dropdown-trigger" data-target="esp">Especialidades
+                            <i class="material-icons right">arrow_drop_down</i>
+                        </a>
+                    </li>
+                    <li><a href="https://compucol.co/colegios/tutaiesannicolas/" target="_blank">Sistema de Notas</a></li>
+                    <li><a href="contacto.php">Contáctenos</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Submenús -->
+        <ul id="institucion" class="dropdown-content">
+            <li><a href="nosotros.php">Reseña Histórica</a></li>
+            <li><a href="simbolos.php">Símbolos Institucionales</a></li>
+            <li><a href="sedes.php">Sedes</a></li>
+            <li><a href="docentes.php">Docentes</a></li>
+            <li><a href="gobierno-escolar.php">Gobierno Escolar</a></li>
+            <li><a href="proyectos.php">Proyectos</a></li>
+            <li><a href="#" target="_blank">Psicoorientación</a></li>
+        </ul>
+        <ul id="esp" class="dropdown-content">
+            <li><a href="redes.php">Redes y Sistemas</a></li>
+            <li><a href="salud.php">Salud</a></li>
+            <li><a href="turismo.php">Turismo</a></li>
+        </ul>
+        <!-- Menú lateral responsivo -->
+        <ul class="sidenav" id="menu-responsive">
+            <li><a href="index.php">Inicio</a></li>
+            <li>
+                <a href="#" class="dropdown-trigger" data-target="institucion-responsive">Institución
+                    <i class="material-icons right">arrow_drop_down</i>
+                </a>
+            </li>
+            <li><a href="live.php" target="_blank">Radio</a></li>
+            <li>
+                <a href="#" class="dropdown-trigger" data-target="esp-responsive">Especialidades
+                    <i class="material-icons right">arrow_drop_down</i>
+                </a>
+            </li>
+            <li><a href="https://compucol.co/colegios/tutaiesannicolas/" target="_blank">Sistema de Notas</a></li>
+            <li><a href="contacto.php">Contáctenos</a></li>
+        </ul>
         </div>
     </header>
